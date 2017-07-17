2f4b958077ef80dc0596a8f80d78b5213d2613a5
wonizz <growuplove7@naver.com>
2017-07-17 22:57:22 +0900

    react commit

---
+++ react-test/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+
+import Counter from './components/Counter';
+import Option from './components/Option';
+import Button from './components/Button';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Counter />
+        <Option />
+        <Button />
+      </div>
+    );
+  }
+}
+
+export default App;
